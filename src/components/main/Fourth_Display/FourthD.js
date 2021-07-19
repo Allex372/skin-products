488a3f374ad0ff8e93c49643060e5f3d8cfff8ed
Allex372 <alexalex372.aa@gmail.com>
2021-07-19 22:45:20 +0300

    init

--- src/components/main/Fourth_Display/FourthD.js
+++ src/components/main/Fourth_Display/FourthD.js
@@ -4,10 +4,8 @@ import img from '../../../img/main/06.png';
 function FourthD() {
     return (
         <div className='fourth-main'>
-            {/*<div style={{margin:'0 auto'}}>*/}
-                <p>Stop accepting the idea that your skin condition isn't curable. Healthy skin is everyone's
-                    birthright.</p>
-            {/*</div>*/}
+            <p>Stop accepting the idea that your skin condition isn't curable. Healthy skin is everyone's
+                birthright.</p>
             <div className='fourth-wrapper'>
                 <img src={img} alt='img'/>
                 <p style={{marginLeft: '22px'}}>Dr. Robyn Kutka,</p>
